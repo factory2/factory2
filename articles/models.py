3694b34e355c208181c651ad93c770887edf814d
Viktor Holovin <holovin@mail.com>
2020-09-11 12:24:46 +0200

    Add null

--- articles/models.py
+++ articles/models.py
@@ -18,8 +18,8 @@ class Article(models.Model):
     code=models.CharField(max_length=200,unique=True)
     title=models.CharField(max_length=200,blank=True)
     description=models.TextField(blank=True)
-    basket=models.ForeignKey(Basket,on_delete=models.CASCADE,blank=True)
-    parameter=models.ForeignKey(Parameter,on_delete=models.CASCADE,blank=True)
+    basket=models.ForeignKey(Basket,on_delete=models.CASCADE,blank=True,null=True)
+    parameter=models.ForeignKey(Parameter,on_delete=models.CASCADE,blank=True,null=True)
 
     def __str__(self):
         return self.code

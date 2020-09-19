d6393823bb67ee8a4bb2b5316a6614dbe3fdb8e0
Viktor Holovin <holovin@mail.com>
2020-09-19 21:13:57 +0200

    Add images

--- articles/models.py
+++ articles/models.py
@@ -4,6 +4,11 @@ class Article(models.Model):
     code=models.SlugField(max_length=50,unique=True)
     title=models.CharField(max_length=200,blank=True)
     description=models.TextField(blank=True)
+    image1=models.ImageField(upload_to='articles',blank=True)
+    image2=models.ImageField(upload_to='articles',blank=True)
+    image3=models.ImageField(upload_to='articles',blank=True)    
+    image4=models.ImageField(upload_to='articles',blank=True)
 
     def __str__(self):
         return self.code
+

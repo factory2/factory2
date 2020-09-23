a87389ee68f7c566516e9e84310d7b123df4b5bb
Viktor Holovin <holovin@mail.com>
2020-09-23 12:48:37 +0200

    Add italy language

--- factory2/settings.py
+++ factory2/settings.py
@@ -45,6 +45,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+#    'django.middleware.locale.LocaleMiddleware',
 ]
 
 ROOT_URLCONF = 'factory2.urls'
@@ -101,7 +102,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'it' #'en-us'
 
 TIME_ZONE = 'UTC'
 
@@ -111,6 +112,9 @@ USE_L10N = True
 
 USE_TZ = True
 
+LOCALE_PATHS = (
+    os.path.join(BASE_DIR, "locale"),
+)
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/

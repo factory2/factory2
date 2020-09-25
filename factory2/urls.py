2a0b421c5dd87502152c5468075bc851eca5dd8b
Viktor Holovin <holovin@mail.com>
2020-09-25 10:27:51 +0200

    Chagne admin name

--- factory2/urls.py
+++ factory2/urls.py
@@ -18,6 +18,10 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+admin.site.site_header = "Administration"
+admin.site.site_title = "Admin Portal"
+admin.site.index_title = "Welcome to Admin Portal"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('articles.urls')),

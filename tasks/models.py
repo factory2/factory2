f3ca2e2ddfe9ba90cdc78bd6bcc0f033b606db71
Debian <admin@ip-172-26-0-232.eu-central-1.compute.internal>
2020-09-27 10:14:45 +0000

    Delete primary key from tasks code

--- tasks/models.py
+++ tasks/models.py
@@ -4,7 +4,7 @@ from articles.models import Article
 from machines.models import BasketDeburring, ParameterDeburring
 
 class ThermalDeburring(models.Model):
-    code = models.OneToOneField(Article, on_delete=models.CASCADE, primary_key=True)
+    code = models.OneToOneField(Article, on_delete=models.CASCADE)
     slug = AutoSlugField(populate_from='code')
     basket_deburring = models.ForeignKey(BasketDeburring,on_delete=models.CASCADE,blank=True,null=True)
     parameter_deburring = models.ForeignKey(ParameterDeburring,on_delete=models.CASCADE,blank=True,null=True)

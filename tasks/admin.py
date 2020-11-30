b59b758a97374f5127f90c77e8282b20717c4422
Viktor Holovin <holovin@mail.com>
2020-11-30 17:48:46 +0100

    Import, export to thermal deburring article

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,4 +1,14 @@
 from django.contrib import admin
 from .models import ThermalDeburring
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
 
-admin.site.register(ThermalDeburring)
+class ThermalDeburringResource(resources.ModelResource):
+
+    class Meta:
+        model = ThermalDeburring
+
+class ThermalDeburringAdmin(ImportExportModelAdmin):
+    resource_class = ThermalDeburringResource
+
+admin.site.register(ThermalDeburring, ThermalDeburringAdmin)

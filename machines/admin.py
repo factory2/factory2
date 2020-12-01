5232154cbe3a5fe26a250d03c592aac52afde43d
Viktor Holovin <holovin@mail.com>
2020-12-01 18:08:57 +0100

    Add import export for all models

--- machines/admin.py
+++ machines/admin.py
@@ -1,7 +1,32 @@
 from django.contrib import admin
-
 from .models import ThermalDeburrer, BasketDeburring, ParameterDeburring
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
 
-admin.site.register(ThermalDeburrer)
-admin.site.register(BasketDeburring)
-admin.site.register(ParameterDeburring)
+class ThermalDeburrerResource(resources.ModelResource):
+
+    class Meta:
+        model = ThermalDeburrer
+
+class ThermalDeburrerAdmin(ImportExportModelAdmin):
+    resource_class = ThermalDeburrerResource
+
+class BasketDeburringResource(resources.ModelResource):
+
+    class Meta:
+        model = BasketDeburring
+
+class BasketDeburringAdmin(ImportExportModelAdmin):
+    resource_class = BasketDeburringResource
+
+class ParameterDeburringResource(resources.ModelResource):
+
+    class Meta:
+        model = ParameterDeburring
+
+class ParameterDeburringAdmin(ImportExportModelAdmin):
+    resource_class = ParameterDeburringResource
+
+admin.site.register(ParameterDeburring, ParameterDeburringAdmin)
+admin.site.register(BasketDeburring, BasketDeburringAdmin)
+admin.site.register(ThermalDeburrer, ThermalDeburrerAdmin)

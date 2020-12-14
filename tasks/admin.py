a079f61f9a2544a6077d19d79640a7cf91985261
Viktor Holovin <holovin@mail.com>
2020-12-14 20:52:37 +0000

    Add pallet thermal deburred

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import ThermalDeburring
+from .models import ThermalDeburring, PalletThermalDeburred
 from import_export import resources
 from import_export.admin import ImportExportModelAdmin
 
@@ -13,3 +13,4 @@ class ThermalDeburringAdmin(ImportExportModelAdmin):
     resource_class = ThermalDeburringResource
 
 admin.site.register(ThermalDeburring, ThermalDeburringAdmin)
+admin.site.register(PalletThermalDeburred)
--- tasks/models.py
+++ tasks/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 from autoslug import AutoSlugField
-from articles.models import Article
+from articles.models import Article, Pallet
 from machines.models import BasketDeburring, ParameterDeburring
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 class ThermalDeburring(models.Model):
     article = models.OneToOneField(Article, primary_key=True, on_delete=models.CASCADE)
@@ -15,3 +17,12 @@ class ThermalDeburring(models.Model):
 
     class Meta:
         ordering = ['article__code']
+
+class PalletThermalDeburred(models.Model):
+    pallet = models.OneToOneField(Pallet, primary_key=True, on_delete=models.CASCADE)
+    thermal_deburred_date = models.DateTimeField(default=timezone.now)
+    employee = models.ForeignKey(User, on_delete = models.CASCADE)
+
+    def __str__(self):
+        date_time = self.thermal_deburred_date.strftime("%d/%m/%Y, %H:%M:%S")
+        return date_time + ", " + self.pallet.article.code

52943f6a384cd3845b66307e1283d3fbd6dff039
Viktor Holovin <holovin@mail.com>
2020-12-20 02:36:31 +0000

    Add field employee to thermaldeburring

--- tasks/models.py
+++ tasks/models.py
@@ -8,9 +8,10 @@ from django.contrib.auth.models import User
 class ThermalDeburring(models.Model):
     article = models.OneToOneField(Article, primary_key=True, on_delete=models.CASCADE)
     slug = AutoSlugField(populate_from='article')
-    basket_deburring = models.ForeignKey(BasketDeburring,on_delete=models.CASCADE,blank=True,null=True)
-    parameter_deburring = models.ForeignKey(ParameterDeburring,on_delete=models.CASCADE,blank=True,null=True)
+    basket_deburring = models.ForeignKey(BasketDeburring, on_delete=models.CASCADE, blank=True, null=True)
+    parameter_deburring = models.ForeignKey(ParameterDeburring, on_delete=models.CASCADE,blank=True,null=True)
     description = models.TextField(blank=True, null=True)
+    employee = models.ForeignKey(User, on_delete = models.CASCADE, blank=True, null=True)
 
     def __str__(self):
         return self.article.code

01e9e627e320e2e33caecfe94380b317b7638bfd
Viktor Holovin <holovin@mail.com>
2020-12-20 02:23:39 +0000

    Add user to article model

--- articles/models.py
+++ articles/models.py
@@ -3,14 +3,15 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 class Article(models.Model):
-    code=models.SlugField(max_length=50,unique=True)
-    title=models.CharField(max_length=200,blank=True, null=True)
+    code=models.SlugField(max_length=50, unique=True)
+    title=models.CharField(max_length=200, blank=True, null=True)
     description=models.TextField(blank=True, null=True)
-    image1=models.ImageField(upload_to='articles',blank=True, null=True)
-    image2=models.ImageField(upload_to='articles',blank=True, null=True)
-    image3=models.ImageField(upload_to='articles',blank=True, null=True) 
-    image4=models.ImageField(upload_to='articles',blank=True, null=True)
+    image1=models.ImageField(upload_to='articles', blank=True, null=True)
+    image2=models.ImageField(upload_to='articles', blank=True, null=True)
+    image3=models.ImageField(upload_to='articles', blank=True, null=True) 
+    image4=models.ImageField(upload_to='articles', blank=True, null=True)
     created_date = models.DateTimeField(default=timezone.now)
+    employee = models.ForeignKey(User, on_delete = models.CASCADE, blank=True, null=True)
 
     def __str__(self):
         return self.code
